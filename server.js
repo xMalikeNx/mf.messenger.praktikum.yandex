f8068c3e8a36f5daae6f170eff00f63a9b2813e1
Vadim Glushchenko <maliken.webdev@gmail.com>
2020-08-11 17:50:16 +0300

    setup express

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('*', (req, res) => {
+  return res.status(404).send('404 not found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
